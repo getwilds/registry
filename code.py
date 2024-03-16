e996078f3c96c5361fc2f2d8cbe35470e97ac023
Scott Chamberlain <sachamber@fredhutch.org>
2024-03-15 21:27:27 -0700

    fix missing topicxs

--- code.py
+++ code.py
@@ -1,4 +1,5 @@
 import json
+import fastcore
 import datetime
 from ghapi.all import GhApi
 
@@ -8,11 +9,17 @@ api = GhApi()
 
 repos = api.repos.list_for_org(ORG)
 
-target_keys = ['name', 'private', 'fork', 'description', 'html_url']
+target_keys = ['name', 'private', 'fork', 'description', 'html_url', 'topics']
 
 data = {}
 data['repos'] = [{key: w[key] for key in target_keys} for w in repos]
 data['updated'] = datetime.datetime.now(datetime.UTC).isoformat()
 
+class LEncoder(json.JSONEncoder):
+	def default(self, obj):
+	   if isinstance(obj, fastcore.foundation.L):
+	      return list(obj)
+	   return json.JSONEncoder.default(self, obj)
+
 with open('registry.json', 'w') as file:
-	json.dump(data, file)
+	json.dump(data, file, cls=LEncoder)
